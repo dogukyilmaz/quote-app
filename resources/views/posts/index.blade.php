05850a7941cf36bcad58452e629ff7c3f4805766
Kaan Yilmaz <douscriptist@gmail.com>
2021-02-28 05:03:20 +0300

    post input field fix

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,26 +1,33 @@
 @extends('layouts.app') @section('content')
 <div class="flex justify-center">
     <div class="w-8/12 bg-gray-700 p-6 rounded-lg">
-        <form action="{{route('posts') }}" method="post" class="mb-4">
-            @csrf
-            <div class="mb-4">
-                <label for="content" class="sr-only">Content</label>
-                <textarea name="content" id="content" cols="30" rows="4" class="bg-gray-500 w-full p-4 rounded-lg @error('content')
-                border-red-500                
-                @enderror"></textarea>
-                @error('content')
-                <div class="text-red-500 mt-2 text-sm">
-                    {{ $message }}
-                </div>
-                @enderror
-            </div>
-
-            <div>
-                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">
-                    Post
-                </button>
-            </div>
-        </form>
+       @auth
+       <form action="{{route('posts') }}" method="post" class="mb-4">
+					@csrf
+					<div class="mb-4">
+							<label for="content" class="sr-only">Content</label>
+							<textarea name="content" id="content" cols="30" rows="4" class="bg-gray-500 w-full p-4 rounded-lg @error('content')
+							border-red-500                
+							@enderror"></textarea>
+							@error('content')
+							<div class="text-red-500 mt-2 text-sm">
+									{{ $message }}
+							</div>
+							@enderror
+					</div>
+					<div>
+							<button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">
+									Post
+							</button>
+					</div>
+    		</form>
+       @endauth
+			 @guest
+			 <div class="mb-4">
+				 <h2 class="text-blue-500"><a href="{{ route('login') }}" class="font-bold">Login</a> to post anything.</h2>
+				</div>
+			 @endguest
+			 
 
         @if ($posts->count())
             @foreach ($posts as $post) 
@@ -31,18 +38,20 @@
                     <p class="mb-2">{{ $post->content }}</p>
 
                     <div class="flex items-center">
-                        @if ($post->likedBy(auth()->user()))
+                        @auth
+                            @if ($post->likedBy(auth()->user()))
                             <form action="{{ route('posts.likes', $post)}}" method="post" class="mr-1">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="text-red-400">Dislike</button>
                             </form>
-                        @else
+                            @else
                             <form action="{{ route('posts.likes', $post)}}" method="post" class="mr-1">
                                 @csrf
                                 <button type="submit" class="text-blue-400">Like</button>
                             </form>
-                        @endif
+                            @endif
+                        @endauth
                         <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
                     </div>
                 </div>
